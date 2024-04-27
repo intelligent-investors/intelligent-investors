fb3ed5fc7a2adb5ad8738c89457830cb20af4c3e
Vu Anh <anhv.ict91@gmail.com>
2024-04-27 08:05:58 +0000

    update

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/intelligent-investors/my-website/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
