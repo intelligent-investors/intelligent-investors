b6eb9d678eb37ec4eb7d4668ea20783979a88d88
Vu Anh <anhv.ict91@gmail.com>
2024-04-27 10:11:12 +0000

    update

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
---
+++ my-website/i18n/vi/docusaurus-plugin-content-docs/current/intro.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro
+
+Xin chào tất cả các bạn.
+
+Hi hi
+
+Xin chào
+
+Xin chào
